34595821bc47567c4fbca464523fd697dd07913e
Antoine REMY <antoine.remy@epita.fr>
2021-05-19 10:46:39 +0200

    part 4 fibo
    Signed-off-by: Antoine REMY <antoine.remy@epita.fr>

--- src/Launcher.java
+++ src/Launcher.java
@@ -7,7 +7,31 @@ public class Launcher {
 
         System.out.println("hi");
         scan = new Scanner(System.in);
-        while (!scan.nextLine().equals("quit"))
-            System.out.println("Unknown command");
+        String test = scan.nextLine();
+        while (!test.equals("quit")) {
+            switch (test) {
+                case "fibo" -> fibo();
+                default -> System.out.println("Unknown command");
+            }
+            test = scan.nextLine();
+        }
+    }
+
+    public static void fibo()
+    {
+        System.out.println("Enter a number N :");
+        int n  = scan.nextInt();
+        int i = n;
+        scan.nextLine();
+        long a = 0;
+        long b = 1;
+        while (i > 0)
+        {
+            long c = b;
+            b = b + a;
+            a = c;
+            i--;
+        }
+        System.out.println(String.format("Fibo(%d)=%d",n,a));
     }
 }
