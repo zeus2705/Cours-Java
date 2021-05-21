3f4e72e212d101fda51ac816eacebaa5eed33b01
Antoine REMY <antoine.remy@epita.fr>
2021-05-21 11:03:43 +0200

    part 7
    Signed-off-by: Antoine REMY <antoine.remy@epita.fr>

--- src/Launcher.java
+++ src/Launcher.java
@@ -70,7 +70,7 @@ class Freq implements Command
             System.out.println();
             return false;
         }
-        List<String> allword = Arrays.asList(file.replaceAll("(,|;|:|'|\n)", " ").toLowerCase().split(" "));
+        List<String> allword = Arrays.asList(file.replaceAll("(,|;|:|'|\n|.)", " ").toLowerCase().split(" "));
         Map<String, Integer> freqmap = new HashMap<>();
         for (String s: allword) {
             if (s.isBlank())
@@ -103,6 +103,83 @@ class Freq implements Command
     }
 }
 
+class Predict implements Command {
+    @Override
+    public String name() {
+        return "predict";
+    }
+
+    @Override
+    public Boolean run(Scanner scan) {
+        System.out.println("Enter the path to the file:");
+        String path = scan.nextLine();
+        String file = "";
+        try {
+            file = java.nio.file.Files.readString(Paths.get(path));
+        } catch (IOException e) {
+            System.out.print(String.format("Unreadable file: %s", e.getClass().getName()));
+            System.out.println();
+            return false;
+        }
+        List<String> allword = Arrays.asList(file.replaceAll("(,|;|:|'|\n)", " ").toLowerCase().split(" "));
+        Map<String, String> nextword = new HashMap<>();
+        for (String target : allword) {
+            Map<String, Integer> freq = new HashMap<>();
+            boolean nextoneok = false;
+            for (String s : allword) {
+                if (s.isBlank())
+                    continue;
+                if (nextoneok) {
+                    Integer count = freq.get(s);
+                    if (count == null)
+                        count = 0;
+                    freq.put(s, count + 1);
+                    nextoneok = s.equals(target);
+                }
+                if (target.equals(s))
+                    nextoneok = true;
+            }
+            for (int k = 0; k < 3; k++) {
+                String best = "";
+                long count = -1;
+                for (Map.Entry<String, Integer> entry : freq.entrySet()) {
+                    if (entry.getValue() > count) {
+                        best = entry.getKey();
+                        count = entry.getValue();
+                    }
+                }
+                if (best.equals(""))
+                    nextword.put(target, ".");
+                else
+                    nextword.put(target, best);
+            }
+
+        }
+        System.out.println("Type a word from the text to start the prediction");
+        String target = scan.nextLine();
+        String[] prediction = new String[20];
+        for (int i = 0; i < 20; i++) {
+            boolean found = false;
+            for (Map.Entry<String, String> entry : nextword.entrySet()) {
+                if (entry.getKey().equals(target)) {
+                    prediction[i] = entry.getValue();
+                    target = prediction[i];
+                    found = true;
+                    break;
+                }
+            }
+            if (!found && i == 0) {
+                System.out.println("Your word was not in the list");
+                return false;
+            }
+            if (prediction[i] == ".")
+                break;
+        }
+        System.out.println(Arrays.stream(prediction).collect(Collectors.joining(" ")));
+        return true;
+    }
+}
+
 public class Launcher {
 
     public static java.util.Scanner scan;
@@ -112,6 +189,7 @@ public class Launcher {
         avaiblecommmand.add(new Fibo());
         avaiblecommmand.add(new Quit());
         avaiblecommmand.add(new Freq());
+        avaiblecommmand.add(new Predict());
         scan = new Scanner(System.in);
         String test = scan.nextLine();
         while (!test.equals("quit")) {
